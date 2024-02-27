b1181e6f8fa82e54b8b2faf88d52886586ce85f3
IvanRyazancev <121186256+IvanRyazancev@users.noreply.github.com>
2024-02-27 23:51:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -8,13 +8,13 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))  # Добавляем столбец для хранения хэшированного пароля
+    password_hash = db.Column(db.String(128))
 
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password_hash = generate_password_hash(password)  # Хэшируем пароль перед сохранением
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
